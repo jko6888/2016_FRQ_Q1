93f3bc93f4595e8ba2e8b408ac84dc8fa2560fc9
BT_1N3_31@bktech.local <BT_1N3_31@bktech.local>
2018-12-21 11:02:24 -0500

    Initial commit

---
+++ src/JamesKo/RandomLetterChooser.java
@@ -0,0 +1,11 @@
+package JamesKo;
+
+public class RandomLetterChooser extends RandomStringChooser {
+    public RandomLetterChooser(String str) {
+        super(RandomLetterChooser.getSingleLetters(str));
+    }
+
+    public static String[] getSingleLetters(String str) {
+        return str.split("");
+    }
+}
---
+++ src/JamesKo/RandomStringChooser.java
@@ -0,0 +1,16 @@
+package JamesKo;
+
+import java.util.ArrayList;
+
+public class RandomStringChooser {
+    private int i = 0;
+    private String[] wordArray;
+
+    public RandomStringChooser(String[] wordArray) {
+        this.wordArray = wordArray;
+    }
+
+    public String getNext() {
+        return this.i < this.wordArray.length ? this.wordArray[this.i++] : "NONE";
+    }
+}
